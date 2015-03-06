f8b0cb2bba70b9765cb5d9ef31bd106ff35c69ec
s. rannou <mxs@sbrk.org>
2015-03-06 08:06:45 +0100

    added parsing of arguments in LabZoo's binaries.

--- labzoo/bin/process.py
+++ labzoo/bin/process.py
@@ -1,5 +1,16 @@
 #-*- coding: utf-8 -*-
 
+import argparse
+
+from labzoo import SessionConfig
+
 
 def main():
-    pass
+    parser = argparse.ArgumentParser(
+       description='process a LabZoo database')
+    parser.add_argument('config', help='path to the YAML file describing the '
+                        'check to run')
+    parser.add_argument('database', help='path to the database file of the '
+                        'session')
+    args = parser.parse_args()
+    session = SessionConfig.load_from_file(args.config)
--- labzoo/bin/run.py
+++ labzoo/bin/run.py
@@ -1,8 +1,17 @@
 #-*- coding: utf-8 -*-
 
+import argparse
+
 from labzoo import SessionConfig
 
 
 def main():
-    session = SessionConfig.load_from_file('hello.yaml')
-    pass
+    parser = argparse.ArgumentParser(
+        description='run LabZoo checks on remote targets'
+    )
+    parser.add_argument('config', help='path to the YAML file describing the'
+                        'check to run')
+    parser.add_argument('database', help='path to the database file of the '
+                        'session')
+    args = parser.parse_args()
+    session = SessionConfig.load_from_file(args.config)
