8848ef15a9de1f90ac4e9e12fd4c91ad095f7ba8
s. rannou <mxs@sbrk.org>
2015-03-23 19:40:29 +0100

    added base flask structure for labzoo-report.

--- labzoo/bin/report.py
+++ labzoo/bin/report.py
@@ -2,7 +2,34 @@
 
 import argparse
 
-from labzoo import SessionConfig
+from flask import (
+    Flask,
+    render_template,
+    request,
+    redirect,
+    url_for,
+    g,
+)
+from labzoo import (
+    Database,
+    SessionConfig,
+)
+
+
+app = Flask(__name__)
+config_path = None
+db_path = None
+
+
+@app.before_request
+def before_request():
+    g.conf = SessionConfig.load_from_file(config_path)
+    g.db_session = Database.load_db(db_path)
+
+
+@app.route('/')
+def home():
+    return render_template('home.html')
 
 
 def main():
@@ -13,4 +40,10 @@ def main():
     parser.add_argument('database', help='path to the database file of the '
                         'session')
     args = parser.parse_args()
-    session = SessionConfig.load_from_file(args.config)
+
+    # set global state
+    global config_path, db_path
+    config_path = args.config
+    db_path = args.database
+
+    app.run('0.0.0.0', debug=True)
